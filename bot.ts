c45d8d6e16646d680dec261af7aab869518c85ac
Federico Secco <federico.secco7@gmail.com>
2023-04-10 01:11:39 +0200

    Changed imports

--- bot.ts
+++ bot.ts
@@ -1,21 +1,20 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const grammy_1 = require("grammy");
-const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-const bot = new grammy_1.Bot(process.env.TELEGRAM_TOKEN || '');
+import { Bot, webhookCallback } from 'grammy';
+import express, { Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const bot = new Bot(process.env.TELEGRAM_TOKEN || '');
+
 // Messages
 const introductionMessage = `Ciao! Sono un bot che selezionerà per te degli esercizi specifici da fare ogni giorno! Gli esercizi sono a cura del dott. Alessandro Mantoan, laureato in bla bla bla. Il messaggio arriverà ogni giorno alle 9!`;
 const helpMessage = `Nessun aiuto a ancora disponibile.`;
 const scheduledMessage1 = `Se ci fossero esericizi, io ora li avrei inviati!`;
+
 const chatIDs = {
     testGroup1: -956704196,
     //testChannel1: -1001859807156,
 };
+
 // start
 bot.command('start', (ctx) => {
     console.log('/start triggered');
@@ -37,13 +36,15 @@ bot.command('test', (ctx) => {
         parse_mode: 'HTML',
     });
 });
+
 bot.command('sendExercises', (ctx) => {
     console.log(`sendExercises triggered`);
     Object.values(chatIDs).forEach((chatID) => {
         bot.api.sendMessage(chatID, scheduledMessage1);
     });
 });
-const logRequest = (req, res, next) => {
+
+const logRequest = (req: Request, res: Response, next: NextFunction) => {
     if (req.method === 'POST' && req.path === '/sendExercises') {
         console.log(`sendExercises triggered`);
         Object.values(chatIDs).forEach((chatID) => {
@@ -52,19 +53,20 @@ const logRequest = (req, res, next) => {
     }
     next();
 };
+
 //deploy
 if (process.env.NODE_ENV === 'production') {
     // Use Webhooks for the production server
-    const app = (0, express_1.default)();
-    app.use(express_1.default.json());
+    const app = express();
+
+    app.use(express.json());
     app.use(logRequest);
-    app.use((0, grammy_1.webhookCallback)(bot, 'express'));
+    app.use(webhookCallback(bot, 'express'));
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
         console.log(`Bot listening on port ${PORT}}`);
     });
-}
-else {
+} else {
     // Use Long Polling for development
     console.log(`Bot working on localhost`);
     bot.start();
